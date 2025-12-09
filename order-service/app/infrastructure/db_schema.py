bcec04e1311d7632eb93538e2158d20df99414e0
iakov2393 <iakov2393@gmail.com>
2025-12-09 21:13:34 +0300

    Revert "Task order microservices"

--- order-service/app/infrastructure/db_schema.py
+++ order-service/app/infrastructure/db_schema.py
@@ -44,14 +44,3 @@ outbox_tbl = Table(
     Column("status", Text, nullable=False),
     Column("created_at", DateTime, server_default=func.now()),
 )
-
-inbox_tbl = Table(
-    "inbox",
-    metadata,
-    Column("id", UUID(as_uuid=True), primary_key=True, default=uuid.uuid4),
-    Column("message_id", Text, nullable=False, unique=True, index=True),
-    Column("event_type", Text, nullable=False),
-    Column("payload", JSON, nullable=False),
-    Column("status", Text, nullable=False),
-    Column("created_at", DateTime, server_default=func.now()),
-)
